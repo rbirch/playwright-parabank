3030616b2340d8f8fee45b6d98d2f23064035e8f
rbirch <jorobi5@outlook.com>
2026-08-13 15:59:08 -0600

    NewAccountPage

---
+++ application/ui/pages/NewAccountPage.ts
@@ -0,0 +1,21 @@
+import { Page, Locator } from '@playwright/test';
+import { PageModel } from '../model/PageModel';
+
+export class NewAccountPage extends PageModel {
+
+    constructor(page: Page) {
+        super(page.locator("div#openAccountForm"));
+    }
+
+    getAccountTypeSelect() : Locator {
+        return this.container.locator("select#type");
+    }
+
+    getFromAccountSelect() : Locator {
+        return this.container.locator("select#fromAccountId");
+    }
+
+    getOpenNewAccountButton() : Locator {
+        return this.container.locator("input[type='submit'][value='Open New Account']");
+    }
+}
